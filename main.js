3b6101c71a24b678ee904ba8273c11d37b4d1f35
chriswebdv <chriswebdv@gmail.com>
2024-08-27 17:02:41 -0600

    use query selector and event listener to grab html elements

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+let addTodoButton = document.querySelector(".addTodo");
+let toDoContainer = document.querySelector("#toDoContainer");
+let inputField = document.querySelector(".inputField");
 
+addTodoButton.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  toDoContainer.appendChild(paragraph);
+});
