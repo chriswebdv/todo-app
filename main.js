fcb1b30a9c0072e5c3293cefe41a3a81501cd8cc
chriswebdv <chriswebdv@gmail.com>
2024-08-27 17:13:58 -0600

    add remove and dbl click to my tasks

--- main.js
+++ main.js
@@ -4,5 +4,14 @@ let inputField = document.querySelector(".inputField");
 
 addTodoButton.addEventListener("click", function () {
   let paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
   toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
 });
